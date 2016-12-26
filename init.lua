0f6ee94dfe859393f280667a73c6c3510ee6115d
NPXcoot <npx_npx@yahoo.it>
2016-12-26 23:14:58 +0100

    Added many fungi

--- init.lua
+++ init.lua
@@ -17,6 +17,101 @@ minetest.register_node("nsspf:boletus_edulis", {
       },
 })
 
+minetest.register_node("nsspf:canthrellus_cibarius", {
+	description = "Cantharellus cibarius",
+	drawtype = "mesh",
+	mesh = "cantharellus_cibarius.b3d",
+    paramtype = 'light',
+    paramtype2 = 'facedir',
+	tiles = {"cantharellus_cibarius.png"},
+	groups = {snappy=3},
+--  drop = 'default:dirt',
+	selection_box = {
+      type = 'fixed',
+      fixed = {-0.28, -0.49, -0.28, 0.28, -0.26, 0.28}, -- Right, Bottom, Back, Left, Top, Front
+      },
+   collision_box = {
+      type = 'fixed',
+      fixed = {-0.28, -0.49, -0.28, 0.28, -0.26, 0.28}, -- Right, Bottom, Back, Left, Top, Front
+      },
+})
+
+minetest.register_node("nsspf:suillus_grevillei", {
+	description = "Suillus grevillei",
+	drawtype = "mesh",
+	mesh = "suillus_grevillei.b3d",
+    paramtype = 'light',
+    paramtype2 = 'facedir',
+	tiles = {"suillus_grevillei.png"},
+	groups = {snappy=3},
+--  drop = 'default:dirt',
+	selection_box = {
+      type = 'fixed',
+      fixed = {-0.28, -0.49, -0.28, 0.28, 0.06, 0.28}, -- Right, Bottom, Back, Left, Top, Front
+      },
+   collision_box = {
+      type = 'fixed',
+      fixed = {-0.28, -0.49, -0.28, 0.28, 0.06, 0.28}, -- Right, Bottom, Back, Left, Top, Front
+      },
+})
+
+minetest.register_node("nsspf:morchella_conica", {
+	description = "Morchella conica",
+	drawtype = "mesh",
+	mesh = "morchella_conica.b3d",
+    paramtype = 'light',
+    paramtype2 = 'facedir',
+	tiles = {"morchella_conica.png"},
+	groups = {snappy=3},
+--  drop = 'default:dirt',
+	selection_box = {
+      type = 'fixed',
+      fixed = {-0.15, -0.49, -0.15, 0.15, 0.06, 0.15}, -- Right, Bottom, Back, Left, Top, Front
+      },
+   collision_box = {
+      type = 'fixed',
+      fixed = {-0.15, -0.49, -0.15, 0.15, 0.06, 0.15}, -- Right, Bottom, Back, Left, Top, Front
+      },
+})
+
+minetest.register_node("nsspf:russula_xerampelina", {
+	description = "Russula xerampelina",
+	drawtype = "mesh",
+	mesh = "russula.b3d",
+    paramtype = 'light',
+    paramtype2 = 'facedir',
+	tiles = {"russula_xerampelina.png"},
+	groups = {snappy=3},
+--  drop = 'default:dirt',
+	selection_box = {
+      type = 'fixed',
+      fixed = {-0.28, -0.49, -0.28, 0.28, 0.06, 0.28}, -- Right, Bottom, Back, Left, Top, Front
+      },
+   collision_box = {
+      type = 'fixed',
+      fixed = {-0.28, -0.49, -0.28, 0.28, 0.06, 0.28}, -- Right, Bottom, Back, Left, Top, Front
+      },
+})
+
+minetest.register_node("nsspf:boletus_pinophilus", {
+	description = "Boletus pinophilus",
+	drawtype = "mesh",
+	mesh = "boletus.b3d",
+    paramtype = 'light',
+    paramtype2 = 'facedir',
+	tiles = {"boletus_pinophilus.png"},
+	groups = {snappy=3},
+--  drop = 'default:dirt',
+	selection_box = {
+      type = 'fixed',
+      fixed = {-0.28, -0.49, -0.28, 0.28, 0.06, 0.28}, -- Right, Bottom, Back, Left, Top, Front
+      },
+   collision_box = {
+      type = 'fixed',
+      fixed = {-0.28, -0.49, -0.28, 0.28, 0.06, 0.28}, -- Right, Bottom, Back, Left, Top, Front
+      },
+})
+
 minetest.register_node("nsspf:boletus_satanas", {
 	description = "Boletus satanas",
 	drawtype = "mesh",
@@ -93,7 +188,7 @@ minetest.register_node("nsspf:macrolepiota_procera", {
       },
 })
 
-function nsspf_register_mycorrhizalmycelium (name, descr, fruit)
+function nsspf_register_mycorrhizalmycelium (name, descr, tree)
 
 minetest.register_node("nsspf:"..name.."_mycelium", {
 	description = descr,
@@ -103,7 +198,7 @@ minetest.register_node("nsspf:"..name.."_mycelium", {
 
 minetest.register_abm({
 	nodenames = {"default:dirt"},
-	neighbors = {"default:tree"},
+	neighbors = {tree},
 	interval = 30.0,
 	chance = 20,
 	action = function(pos, node)
@@ -166,15 +261,20 @@ minetest.register_abm({
 end
 
 
-nsspf_register_mycorrhizalmycelium ('amanita_muscaria','Amanita muscaria Mycelium')
-nsspf_register_mycorrhizalmycelium ('amanita_phalloides','Amanita phalloides Mycelium')
-nsspf_register_mycorrhizalmycelium ('boletus_edulis','Boletus edulis Mycelium')
-nsspf_register_mycorrhizalmycelium ('boletus_satanas','Boletus satanas mycelium')
+nsspf_register_mycorrhizalmycelium ('amanita_muscaria','Amanita muscaria Mycelium', "default:pine_tree")
+nsspf_register_mycorrhizalmycelium ('amanita_phalloides','Amanita phalloides Mycelium', "default:pine_tree")
+nsspf_register_mycorrhizalmycelium ('boletus_edulis','Boletus edulis Mycelium', "default:tree")
+nsspf_register_mycorrhizalmycelium ('boletus_satanas','Boletus satanas Mycelium', "default:tree")
+nsspf_register_mycorrhizalmycelium ('boletus_pinophilus','Boletus pinophilus Mycelium', "default:pine_tree")
+nsspf_register_mycorrhizalmycelium ('suillus_grevillei','Suillus grevillei Mycelium','default:pine_tree')
+nsspf_register_mycorrhizalmycelium ('russula_xerampelina','Russula xerampelina Mycelium', "default:pine_tree")
+nsspf_register_mycorrhizalmycelium ('cantharellus_cibarius','Cantherellus cibarius Mycelium', "default:pine_tree")
+nsspf_register_mycorrhizalmycelium ('morchella_conica','Morchella conica Mycelium', "default:aspen_tree")
 --nsspf_register_mycelium ('macrolepiota_procera_mycelium','')
 --nsspf_register_mycelium ('','')
 
 
---tuber: il frutto, il cubo che lo droppa, il micelio, il micelio morto, le spore
+--tuber
 function nsspf_register_tuber (name, descr, tree)
 
 minetest.register_node("nsspf:"..name.."_tuber_mycelium", {
@@ -322,4 +422,129 @@ minetest.register_node("nsspf:terfezia_arenaria_fungusdirt", {
 		}
 	},
 	groups = {crumbly = 3},
+})
+
+--Saprotrophic fungi that live on trees
+minetest.register_node("nsspf:fistulina_hepatica", {
+	description = "Fistulina hepatica",
+	drawtype = "mesh",
+	mesh = "fistulina_hepatica.b3d",
+    paramtype = 'light',
+    paramtype2 = 'facedir',
+	tiles = {"fistulina_hepatica.png"},
+	groups = {snappy=3},
+--  drop = 'default:dirt',
+	selection_box = {
+      type = 'fixed',
+      fixed = {-0.5, -0.1, -0.5, 0.5, 0.1, 0.5}, -- Right, Bottom, Back, Left, Top, Front
+      },
+   collision_box = {
+      type = 'fixed',
+      fixed = {-0.5, -0.1, -0.5, 0.5, 0.1, 0.5}, -- Right, Bottom, Back, Left, Top, Front
+      },
+})
+
+minetest.register_node("nsspf:armillaria_mellea", {
+	description = "Armillaria mellea",
+	drawtype = "mesh",
+	mesh = "armillaria_mellea.b3d",
+    paramtype = 'light',
+    paramtype2 = 'facedir',
+	tiles = {"armillaria_mellea.png"},
+	groups = {snappy=3},
+--  drop = 'default:dirt',
+	selection_box = {
+      type = 'fixed',
+      fixed = {-0.2, 0, 0.2, 0.2, 0.4, 0.5}, -- Right, Bottom, Back, Left, Top, Front
+      },
+   collision_box = {
+      type = 'fixed',
+      fixed = {-0.2, 0, 0.2, 0.2, 0.4, 0.5}, -- Right, Bottom, Back, Left, Top, Front
+      },
+})
+
+minetest.register_node("nsspf:panellus_pusillus", {
+	description = "Panellus pusillus",
+	drawtype = "mesh",
+	mesh = "panellus_pusillus.b3d",
+    paramtype = 'light',
+    paramtype2 = 'facedir',
+	tiles = {"panellus_pusillus.png"},
+	groups = {snappy=3},
+--  drop = 'default:dirt',
+	selection_box = {
+      type = 'fixed',
+      fixed = {-0.2, -0.4, 0.2, 0.2, 0.4, 0.5}, -- Right, Bottom, Back, Left, Top, Front
+      },
+   collision_box = {
+      type = 'fixed',
+      fixed = {-0.2, -0.4, 0.2, 0.2, 0.4, 0.5}, -- Right, Bottom, Back, Left, Top, Front
+      },
+})
+
+minetest.register_node("nsspf:panellus_pusillus_light", {
+	description = "Nocturn Panellus pusillus",
+	drawtype = "mesh",
+	mesh = "panellus_pusillus.b3d",
+    paramtype = 'light',
+    paramtype2 = 'facedir',
+	tiles = {"panellus_pusillus_light.png"},
+	groups = {snappy=3},
+	light_source = 10,
+--  drop = 'default:dirt',
+	selection_box = {
+      type = 'fixed',
+      fixed = {-0.2, -0.4, 0.2, 0.2, 0.4, 0.5}, -- Right, Bottom, Back, Left, Top, Front
+      },
+   collision_box = {
+      type = 'fixed',
+      fixed = {-0.2, -0.4, 0.2, 0.2, 0.4, 0.5}, -- Right, Bottom, Back, Left, Top, Front
+      },
+})
+
+--spawna random sui tronchi, controlli
+
+function nssbf_register_saprotrophicontrees (name, descr, tree)
+
+minetest.register_abm({
+	nodenames = {tree},
+	neighbors = {"air"},
+	interval = 10.0,
+	chance = 20,
+	action = function(pos, node)
+		local pos1 = {x=pos.x, y=pos.y, z=pos.z-1}
+		local n = minetest.env:get_node(pos1).name
+		if n == 'air' then
+			minetest.set_node({x = pos.x, y = pos.y, z = pos.z-1}, {name = "nsspf:"..name})
+		end
+	end
+})
+
+end
+
+nssbf_register_saprotrophicontrees ('armillaria_mellea', 'Armillaria mellea', 'default:tree')
+nssbf_register_saprotrophicontrees ('panellus_pusillus', 'Panellus pusillus', 'default:tree')
+nssbf_register_saprotrophicontrees ('fistulina_hepatica', 'Fistulina hepatica', 'default:tree')
+
+local OPEN_TIME_START = 0.2 -- Day time at which moon flowers open up
+local OPEN_TIME_END = 0.8 -- Day time at which moon flowers close up
+local OPEN_CHECK = 10 -- Interval at which to check if lighting changed
+
+set_fl = function (pos)
+	if (minetest.env:get_node_light(pos, 0.5) == 15)
+	and ((minetest.env:get_timeofday() < OPEN_TIME_START) or (minetest.env:get_timeofday() > OPEN_TIME_END)) then
+		minetest.env:add_node(pos, { name = "nsspf:panellus_pusillus" })
+	else
+		minetest.env:add_node(pos, { name = "nsspf:panellus_pusillus_light" })
+	end
+end
+
+minetest.register_abm({
+	nodenames = { "nsspf:panellus_pusillus", "nsspf:panellus_pusillus_light" },
+	interval = OPEN_CHECK,
+	chance = 1,
+
+	action = function(pos, node, active_object_count, active_object_count_wider)
+		set_fl(pos)
+	end
 })
