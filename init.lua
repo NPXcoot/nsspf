e86c2e8ddf292ca3bb367f9e2347882de26ddf1f
mckaygerhard <mckaygerhard@gmail.com>
2023-06-18 12:01:09 -0400

    loading order fix, for correct spawning, added information files
    * add files for description and dependencies on modern engines
    * add dependencies info into README
    * use mod namespaced for detection of nnsm and tnt
    * add tnt and nssm as optional dependencies

--- init.lua
+++ init.lua
@@ -1,4 +1,15 @@
-local path = minetest.get_modpath("nsspf")
+nsspf = {}
+local mod = nsspf
 
-dofile(path.."/spawn.lua")
-dofile(path.."/mushrooms.lua")
+mod.version = '20230616'
+mod.mod_name = minetest.get_current_modname()
+mod.path = minetest.get_modpath(mod.mod_name)
+mod.world = minetest.get_worldpath()
+mod.nssm = minetest.get_modpath("nssm")
+mod.tnt = minetest.get_modpath("tnt")
+mod.dat = {}
+
+dofile(mod.path.."/mushrooms.lua")
+dofile(mod.path.."/spawn.lua")
+
+print("[MOD] loaded nsspf")
